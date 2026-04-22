c97f5030e7b61698a04002a1b6144e3f893c3d4c
Anthony Nino <anthonyninodev@gmail.com>
2026-04-22 00:08:53 -0400

    Botton disactive

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { transferMoney } from '../controllers/walletController.js';
+import { toggleAccountStatus } from '../controllers/StatusController.js';
 
 const router = Router();
 
 router.post('/transfer', transferMoney);
+router.patch('/accounts/:id/toggle-status', toggleAccountStatus);
 
 export default router;
